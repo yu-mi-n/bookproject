283fc1201db976dad4e89cdbca4285ffc6d68e03
yu-mi-n <igawa.yuito0818@gmail.com>
2025-12-10 10:09:33 +0900

    first commit

---
+++ accounts/forms.py
@@ -0,0 +1,7 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+
+class SignupForm(UserCreationForm):
+  class meta:
+    model = User
+    fields = ('username')
